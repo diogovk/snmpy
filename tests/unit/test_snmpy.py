037974cb51764a475b7d7590178130ae9cd20540
Klaus Laube <klaus@corp.globo.com>
2011-06-08 14:16:10 -0300

    Added fudge to fake subprocess instructions

--- tests/unit/test_snmpy.py
+++ tests/unit/test_snmpy.py
@@ -1,13 +1,36 @@
+import fudge
 import unittest
 
 from snmpy import Snmpy
 
+
 class SnmpyTest(unittest.TestCase):
     def setUp(self):
-        self.snmpy = Snmpy(dest_host='127.0.0.1', community='public')
+        self.dest_host = '127.0.0.1'
+        self.community = 'public'
+        self.version = '2c'
+        self.snmpy = Snmpy(self.dest_host, self.community, self.version)
 
-    def test_walk(self):
-        response = self.snmpy.walk('ifDescr')
+    @fudge.patch('subprocess.Popen')
+    def test_walk_with_ifdescr(self, FakePopen):
+        oid = 'ifDescr'
+
+        # Fake subprocess command
+        fake_response = fudge.Fake('response').is_a_stub()
+        fake_response.stdout = (fudge.Fake('stdout')
+            .provides('read')
+           .returns("""ifDescr.1 = STRING: lo
+           ifDescr.2 = STRING: eth0
+           ifDescr.3 = STRING: wlan0
+           """)
+        )
+        (FakePopen.expects_call()
+            .with_args(('snmpwalk', '-Os', '-v', self.version, '-c',
+                self.community, self.dest_host, oid), stdout=-1, stderr=-1)
+            .returns(fake_response)
+        )
+
+        response = self.snmpy.walk(oid)
         snmpwalk = [
             {'group': 'ifDescr', 'index': '1', 'type': 'STRING',
                 'value': 'lo'},
