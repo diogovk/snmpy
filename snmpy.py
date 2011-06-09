f3180df5c2685a86098c0a7d595732bdebc40d0d
Klaus Laube <klaus@corp.globo.com>
2011-06-09 19:34:34 -0300

    Refactored NetworkInterfaces class

--- snmpy.py
+++ snmpy.py
@@ -47,11 +47,76 @@ class Snmpy(object):
 
         return result
 
-    def walk(self, oid):
+    def walk(self, oid, first_value=True):
         """
         Executes SNMPWALK to read values from SNMP.
         """
         result = self._subprocess(('snmpwalk', '-Os', '-v', self._version,
             '-c', self._community, self._dest_host, oid))
 
-        return self._filter(result)
+        return self._filter(result, first_value)
+
+    def get(self, oid, first_value=True):
+        """
+        Executes SNMPGET to get values from SNMP.
+        """
+        try:
+            result = self._subprocess(('snmpget', '-Os', '-v', self._version,
+                '-c', self._community, self._dest_host, oid))
+        except TypeError:
+            return {}
+
+        return self._filter(result, first_value)
+
+
+class NetworkInterfaces(Snmpy):
+    """
+    Specific SNMP instructions to get iface informations.
+    """
+
+    oid = 'ifDescr'
+    _all_elements = {
+        'status': 'ifOperStatus',
+        'speed': 'ifSpeed',
+        'in_oct': 'ifInOctets',
+        'in_discards': 'ifInDiscards',
+        'in_errors': 'ifInErrors',
+        'out_oct': 'ifOutOctets',
+        'out_discards': 'ifOutDiscards',
+        'out_errors': 'ifOutErrors',
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(NetworkInterfaces, self).__init__(*args, **kwargs)
+
+        # Executes SNMPWALK with specific OID to grab ifaces information
+        self.walk = self.walk(self.oid, first_value=False)
+        self._ifaces = self._get_ifaces()
+
+    def _get_ifaces(self):
+        """
+        Regroups SNMPWALK result and get iface details.
+        """
+
+        ifaces = {}
+
+        for i in self.walk:
+            iface = i['value']
+            index = i['index']
+            ifaces.setdefault(iface, {})
+
+            for j in self._all_elements:
+                value = self.get(self._all_elements[j] + '.' + index)
+                ifaces[iface][j] = value[0]['value']
+
+        return ifaces
+
+    def __getitem__(self, items):
+        """
+        Adds dict behaviour to NetworkInterfaces instances.
+        """
+
+        if type(items) != str or items not in self._ifaces:
+            raise KeyError
+
+        return self._ifaces[items]
--- tests/unit/test_snmpy.py
+++ tests/unit/test_snmpy.py
@@ -9,26 +9,33 @@ class SnmpyTest(unittest2.TestCase):
         self.dest_host = '127.0.0.1'
         self.community = 'public'
         self.version = '2c'
+
         self.snmpy = Snmpy(self.dest_host, self.community, self.version)
+
+    def _fake_subprocess(self, Class_, snmp_command, version, community,
+            dest_host, oid, returns):
+        fake_response = fudge.Fake('response').is_a_stub()
+        fake_response.stdout = (fudge.Fake('stdout')
+            .provides('read')
+            .returns(returns)
+        )
+
+        (Class_.expects_call()
+            .with_args((snmp_command, '-Os', '-v', version, '-c', community,
+                dest_host, oid), stdout=-1, stderr=-1)
+            .returns(fake_response)
+        )
 
     @fudge.patch('subprocess.Popen')
     def test_walk_with_ifdescr(self, FakePopen):
         oid = 'ifDescr'
 
         # Fake subprocess command
-        fake_response = fudge.Fake('response').is_a_stub()
-        fake_response.stdout = (fudge.Fake('stdout')
-            .provides('read')
-           .returns("""ifDescr.1 = STRING: lo
-           ifDescr.2 = STRING: eth0
-           ifDescr.3 = STRING: wlan0
-           """)
-        )
-        (FakePopen.expects_call()
-            .with_args(('snmpwalk', '-Os', '-v', self.version, '-c',
-                self.community, self.dest_host, oid), stdout=-1, stderr=-1)
-            .returns(fake_response)
-        )
+        self._fake_subprocess(FakePopen, 'snmpwalk', self.version,
+            self.community, self.dest_host, oid, """ifDescr.1 = STRING: lo
+            ifDescr.2 = STRING: eth0
+            ifDescr.3 = STRING: wlan0
+        """)
 
         response = self.snmpy.walk(oid)
         snmpwalk = [
@@ -41,3 +48,21 @@ class SnmpyTest(unittest2.TestCase):
         ]
 
         self.assertEquals(response, snmpwalk)
+
+    @fudge.patch('subprocess.Popen')
+    def test_get_with_ifdescr(self, FakePopen):
+        oid = 'ifDescr.1'
+
+        # Fake subprocess command
+        self._fake_subprocess(FakePopen, 'snmpget', self.version,
+            self.community, self.dest_host, oid, 'ifDescr.1 = STRING: lo')
+
+        response = self.snmpy.get(oid)
+        snmpget = [{
+            'index': '1',
+            'group': 'ifDescr',
+            'value': 'lo',
+            'type': 'STRING',
+        }]
+
+        self.assertEquals(response, snmpget)
